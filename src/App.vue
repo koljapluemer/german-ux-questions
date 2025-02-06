2b0f85f8e885c77a71bc98e7389c95957e14aac3
blackbrokkoli <kastomat@gmail.com>
2025-02-06 20:43:36 +0100

    clear obsolete header

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,6 @@
 <template>
   <header>
     <nav class="flex flex-row gap-2 m-auto justify-center">
-      <router-link class="btn btn-sm" to="/">New Image</router-link>
-      <router-link class="btn btn-sm" to="/words">My Words</router-link>
-      <router-link class="btn btn-sm" to="/learn">Practice</router-link>
     </nav>
   </header>
   <main class="m-auto p-4 flex flex-col max-w-2xl">
